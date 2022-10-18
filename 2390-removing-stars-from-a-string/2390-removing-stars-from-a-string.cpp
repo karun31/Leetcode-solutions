3b4ad3c108039b4060af3fcfe63353fd980940c6
Karun <107580357+karun31@users.noreply.github.com>
2022-10-18 21:02:27 +0530

    Time: 257 ms (12.23%), Space: 27 MB (37.46%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
-    
-    string helper(string s){
-        int i = 0;
-        while(i < s.length() && s[i] != '*'){
-            i++;
-        }
-        if(i == s.length()){
-            return s;
-        }
-        i--;
-        string newS = "";
-        for(int x = 0; x < i; x++){
-            newS += s[x];
-        }
-        for(int x = i+2; x < s.length(); x++){
-            newS += s[x];
-        }
-        
-        return helper(newS);
-        
-    }
-    
-    
+   
     string removeStars(string s) {
         stack<char> S;
         for(int i = 0; i < s.length(); i++){
