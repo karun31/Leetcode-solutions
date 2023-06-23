55ec0c64a361b7330342a81353a3ad6e05dde7eb
Karun <107580357+karun31@users.noreply.github.com>
2023-06-23 12:09:01 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,63 +7,31 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	//DFS
-// 	void fillStack(int n, vector<int> adj[],stack<int>& s, vector<bool>& visited, int si){
-// 	    visited[si] = true;
-// 	    for(auto adjVertex : adj[si]){
-// 	        if(!visited[adjVertex]){
-// 	            fillStack(n,adj,s,visited,adjVertex);
-// 	        }
-// 	    }
-// 	    s.push(si);
-// 	}
 	
-// 	vector<int> topoSort(int V, vector<int> adj[]) 
-// 	{
-// 	    // code here
-// 	    vector<int> ans;
-// 	    stack<int> s;
-// 	    vector<bool> visited(V,false);
-// 	    for(int i = 0; i < V; i++){
-// 	        if(!visited[i]){
-// 	            fillStack(V,adj,s,visited,i);
-// 	        }
-// 	    }
-	    
-// 	    while(!s.empty()){
-// 	        ans.push_back(s.top());
-// 	        s.pop();
-// 	    }
-// 	    return ans;
-// 	}
-	
-	
-	//BFS
-	vector<int> topoSort(int V, vector<int> adj[]){
-	    vector<int> indegree(V,0);
-	    for(int i = 0; i < V; i++){
-	        for(auto adjVertex: adj[i]){
-	            indegree[adjVertex]++;
+	void topoSortUsingDFS(int V, vector<int> adj[], int sv, vector<bool>& visited, stack<int>& s){
+	    visited[sv] = true;
+	    for(auto adjVertex : adj[sv]){
+	        if(!visited[adjVertex]){
+	            topoSortUsingDFS(V,adj,adjVertex,visited,s);
 	        }
 	    }
-	    
-	    queue<int> q;
+	    s.push(sv);
+	}
+	
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    stack<int> s;
+	    vector<bool> visited(V,false);
 	    for(int i = 0; i < V; i++){
-	        if(indegree[i] == 0){
-	            q.push(i);
+	        if(!visited[i]){
+	            topoSortUsingDFS(V,adj,i,visited,s);
 	        }
 	    }
 	    vector<int> ans;
-	    while(!q.empty()){
-	        int node = q.front();
-	        q.pop();
-	        ans.push_back(node);
-	        for(auto adjVertex : adj[node]){
-	            indegree[adjVertex]--;
-	            if(indegree[adjVertex] == 0){
-	                q.push(adjVertex);
-	            }
-	        }
+	    while(!s.empty()){
+	        ans.push_back(s.top());
+	        s.pop();
 	    }
 	    return ans;
 	}
