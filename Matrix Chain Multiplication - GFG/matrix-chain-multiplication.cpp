613758c63cf5c10f31091b2d0ca1a4f824ad8e51
Karun <107580357+karun31@users.noreply.github.com>
2023-07-09 13:27:57 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -40,10 +40,31 @@ public:
         }
         return memo[si][ei] = minVal;
     }
+    
+    int dpHelp(int arr[], int n){
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int si = n-2; si >= 0; si--){
+            for(int ei = 0; ei <= n-2; ei++){
+                if(si >= ei){
+                    continue;
+                }
+                int minVal = INT_MAX;
+                for(int k = si; k < ei; k++){
+                    int lf = dp[si][k];
+                    int rf = dp[k+1][ei];
+                    int s = arr[si]*arr[k+1]*arr[ei+1];
+                    minVal = min(minVal,lf+rf+s);
+                }
+                dp[si][ei] = minVal;
+            }
+        }
+        return dp[0][n-2];
+    }
 
     int matrixMultiplication(int N, int arr[])
     {
         // code here
+        return dpHelp(arr,N);
         vector<vector<int>> memo(N-1,vector<int>(N-1,-1));
         return memoHelp(arr,N,0,N-2,memo);
         //return recurHelp(arr,N,0,N-2);
