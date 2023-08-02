02dd1aa5b43791c5e77183bd1a5c488fb857fac9
Karun <107580357+karun31@users.noreply.github.com>
2023-08-02 17:28:21 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -10,22 +10,23 @@ using namespace std;
 class Solution{
 public:
 
-    int recurHelp(int arr[], int N, int si, int ei){
-        if(si == ei){
+
+    int recurHelp(int N, int arr[], int si, int ei){
+        if(si >= ei){
             return 0;
         }
         int minVal = INT_MAX;
         for(int k = si; k < ei; k++){
-            int lf = recurHelp(arr,N,si,k);
-            int rf = recurHelp(arr,N,k+1,ei);
-            int s = arr[si]*arr[k+1]*arr[ei+1];
-            minVal = min(minVal,lf+rf+s);
+            int lVal = recurHelp(N,arr,si,k);
+            int rVal = recurHelp(N,arr,k+1,ei);
+            int multiVal = arr[si]*arr[k+1]*arr[ei+1];
+            minVal = min(minVal,lVal+rVal+multiVal);
         }
         return minVal;
     }
     
-    int memoHelp(int arr[], int N, int si, int ei,vector<vector<int>>& memo){
-        if(si == ei){
+    int memoHelp(int N, int arr[], int si, int ei, vector<vector<int>>& memo){
+        if(si >= ei){
             return 0;
         }
         if(memo[si][ei] != -1){
@@ -33,41 +34,20 @@ public:
         }
         int minVal = INT_MAX;
         for(int k = si; k < ei; k++){
-            int lf = memoHelp(arr,N,si,k,memo);
-            int rf = memoHelp(arr,N,k+1,ei,memo);
-            int s = arr[si]*arr[k+1]*arr[ei+1];
-            minVal = min(minVal,lf+rf+s);
+            int lVal = memoHelp(N,arr,si,k,memo);
+            int rVal = memoHelp(N,arr,k+1,ei,memo);
+            int multiVal = arr[si]*arr[k+1]*arr[ei+1];
+            minVal = min(minVal,lVal+rVal+multiVal);
         }
         return memo[si][ei] = minVal;
-    }
-    
-    int dpHelp(int arr[], int n){
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int si = n-2; si >= 0; si--){
-            for(int ei = 0; ei <= n-2; ei++){
-                if(si >= ei){
-                    continue;
-                }
-                int minVal = INT_MAX;
-                for(int k = si; k < ei; k++){
-                    int lf = dp[si][k];
-                    int rf = dp[k+1][ei];
-                    int s = arr[si]*arr[k+1]*arr[ei+1];
-                    minVal = min(minVal,lf+rf+s);
-                }
-                dp[si][ei] = minVal;
-            }
-        }
-        return dp[0][n-2];
     }
 
     int matrixMultiplication(int N, int arr[])
     {
+        vector<vector<int>> memo(N,vector<int>(N,-1));
+        return memoHelp(N,arr,0,N-2,memo);
+        return recurHelp(N,arr,0,N-2);
         // code here
-        return dpHelp(arr,N);
-        vector<vector<int>> memo(N-1,vector<int>(N-1,-1));
-        return memoHelp(arr,N,0,N-2,memo);
-        //return recurHelp(arr,N,0,N-2);
     }
 };
 
